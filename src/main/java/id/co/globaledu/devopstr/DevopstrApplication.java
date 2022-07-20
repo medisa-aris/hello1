fe23ff0c43ed5146661169e908558ee87a0660f5
medisa.aris <medisa.aris@fusi24.com>
2022-07-20 14:57:34 +0700

    initial commit

---
+++ src/main/java/id/co/globaledu/devopstr/DevopstrApplication.java
@@ -0,0 +1,13 @@
+package id.co.globaledu.devopstr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopstrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopstrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/co/globaledu/devopstr/DevopstrApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.globaledu.devopstr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopstrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
